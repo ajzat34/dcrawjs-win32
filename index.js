9c124d8e061612d29c9b373118e7b7c42486d278
Andrew Johnson <ajzat25@gmail.com>
2020-05-06 01:04:02 -0400

    moved wrapper to dcrawjs

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-const { spawn } = require('child_process')
 const path = require('path')
-
-module.exports = function(source) {
-  return new Promise(function(resolve, reject){
-    try {
-      const sh = spawn(path.join(__dirname + '/dcraw'), ['-T','-6','-c',source])
-      let chunks = []
-      sh.stdout.on('data', (chunk) => {
-        chunks.push(chunk)
-      })
-      sh.stderr.on('data', (data) => {
-        console.log(data.toString())
-      })
-      sh.on('close', () => {
-        resolve(Buffer.concat(chunks))
-      })
-    } catch(err){
-      reject(err)
-    }
-  })
-}
+module.exports = path.join(__dirname + '/dcraw')
